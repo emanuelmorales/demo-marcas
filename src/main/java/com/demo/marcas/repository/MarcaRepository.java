4b561d76f374e0dfc835bd967540471e56d7368e
Emanuel Morales <moralesgomez.emanuel@gmail.com>
2024-05-16 22:41:31 -0300

    feature/ema-2 feat: se agregó servicio de consulta de marcas, MarcaRepository y conexión a la base de datos

--- src/main/java/com/demo/marcas/repository/MarcaRepository.java
+++ src/main/java/com/demo/marcas/repository/MarcaRepository.java
@@ -2,9 +2,11 @@ package com.demo.marcas.repository;
 
 import com.demo.marcas.model.Marca;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MarcaRepository extends JpaRepository <Marca, Integer> {
+    @Query("select m from Marca m where fechaBaja is null")
     List<Marca> findByFechaBajaIsNull();
 }
